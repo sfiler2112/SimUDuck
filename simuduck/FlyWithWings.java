51d0ee6d5bd7f1da0c292a02cdaac14426b40c95
sfiler2112 <42945574+sfiler2112@users.noreply.github.com>
2019-10-27 11:43:03 -0600

    Add files via upload

---
+++ simuduck/FlyWithWings.java
@@ -0,0 +1,7 @@
+package simuduck;
+
+public class FlyWithWings implements FlyBehavior {
+    public void fly(){
+        System.out.println("I'm flying!");
+    }
+}
---
+++ simuduck/MiniDuckSimulator.java
@@ -0,0 +1,16 @@
+package simuduck;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args){
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+        double a = Math.pow(2,2);
+        System.out.println(a);
+    }
+}
